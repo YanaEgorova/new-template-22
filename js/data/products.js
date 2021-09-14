071ff6f2e3fbd4f19914b1c9eb5d43468b20a67b
Yana <yanaegorova69.3@gmail.com>
2021-09-14 18:10:42 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,155 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Stroller Travel Accessory Set',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Use links to hang small baby toys from carriers, strollers, cribs, and diaper bags.',
+            },
+            {
+                text: 'The 12 colored links are also great for teething.',
+            },
+            {
+                text: 'Custom S-shaped hanging link is designed so toys face toward baby.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: '6-Piece Towel Set',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img2.png',
+        description: [
+            {
+                text: '100% Cotton',
+            },
+            {
+                text: '6-piece bath and hand towel set; ideal for any master or guest bathroom.',
+            },
+            {
+                text: 'Includes 2 bath towels, 2 hand towels, and 2 washcloths made of 100% sustainable BCI cotton; 425 GSM.',
+            },
+            {
+                text: 'Lightweight and absorbent with double stitched hems for a cozy feel and added strength.',
+            },
+            {
+                text: 'Fade resistant dark blue color; 2 unique designs per set (dots and blocks).',
+            },
+            {
+                text: 'Product dimensions: 30 x 54 inches (bath towel), 16 x 26 inches (hand towel), and 12 x 12 inches (washcloth).',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.jpeg'],
+    },
+    {
+        id: 'product-3',
+        name: '6-Light Chandelier',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Classic Meets Contemporary',
+                text: 'Classic white shades get a glossy upgrade for a contemporary feel that pairs perfectly with the dramatic dark bronze frame.',
+            },
+            {
+                title: 'Versatile Placement',
+                text: 'Includes 4 stems of varying lengths for a range of hanging heights; 12. 61" minimum, 52. 42" maximum length. The pivot point below the canopy makes this fixture easy to hang anywhere, even sloped ceilings.',
+            },
+            {
+                title: 'Dimmable',
+                text: 'Completely dimmable with a compatible dimmer switch to create the perfect ambience.',
+            },
+            {
+                title: 'Easy-To-Install',
+                text: 'All mounting hardware is included for quick and easy installation.',
+            },
+            {
+                title: 'Bulb Information',
+                text: 'Requires six 60W E26 bulbs or equivalents (sold separately).',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.jpg', './img/img3-4.jpg'],
+    },
+    {
+        id: 'product-4',
+        name: 'Wall Clock',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Round silver plastic case.',
+            },
+            {
+                text: 'Easy to read black numerals.',
+            },
+            {
+                text: 'Precise quartz movement.',
+            },
+            {
+                text: 'Powered by 1 AA battery (included).',
+            },
+            {
+                text: 'Overall dimensions: 20 X X 1.8',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: '3 Tier Cart',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Beautiful And Versatile',
+                text: 'Designed for Kitchen, Garage, Play, Family or Kids Room - Get Organized.',
+            },
+            {
+                title: 'Mobile Utility Cart',
+                text: 'Effortless Easy Movement or Sturdy Freestanding Cart With Two Locking Casters.',
+            },
+            {
+                title: 'Durability And Functional',
+                text: 'Steel constructed frame, heavy load casters, long-lasting and beautiful. Perfect For - Catering, Outdoor Parties, BBQ\'s Or As A Metal Utility Cart or Wheeled Cart.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Portable Magnetic Mini Cycle',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Adjustable Resistance',
+                text: 'This bike is built with a manual resistance knob that allows every user to easily adjust the resistance for a more customized workout. Its magnetic resistance offers smooth transitions between levels for uninterrupted training.',
+            },
+            {
+                title: 'Portable And Compact Equipment',
+                text: 'Strategically engineered with a space-saving design that\'s perfect for any home gym, this pedal machine’s structure is built to give every user the convenience of performing workouts anytime, anywhere, even in the office!',
+            },
+            {
+                title: 'Digital Tracking',
+                text: 'Improved with an easy-to-read computer display that tracks your workout throughout your training session, this mini cycle machine also features a handy scan mode, allowing you to conveniently review your stats and monitor your progress.',
+            },
+            {
+                title: 'Upper- And Lower-Body Workout',
+                text: 'Tone your body and achieve that dream physique without worries! This fitness gear is designed to offer both upper- and lower-body training. The maximum weight capacity of 250 lbs. makes it ideal for various user weights.',
+            },
+            {
+                title: 'Reliable And Secure',
+                text: 'Give yourself a worry-free exercise session. This apparatus features rubberized feet designed to increase stability and prevent the equipment from unnecessary movements. It features grooved pedals for maximum hand and foot grip.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png'],
+    },
+]
